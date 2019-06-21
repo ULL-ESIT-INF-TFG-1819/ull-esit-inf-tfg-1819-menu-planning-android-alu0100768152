0c6918e9b3773aa3dce2aa431c412d652cf57eb8
Óscar Socas González <alu0100768152@ull.edu.es>
2019-06-21 13:27:02 +0100

    Generando el menu con todas las caracteristicas necesarias

--- code/TFG3/app/src/main/java/com/example/tfg3/CrearMenu.java
+++ code/TFG3/app/src/main/java/com/example/tfg3/CrearMenu.java
@@ -17,7 +17,19 @@ import com.example.tfg3.CarpetaAPI.RecipeService;
 import com.example.tfg3.CarpetaAPI.Respuesta;
 import com.example.tfg3.SQLite.AdminSQLiteOpenHelper;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,6 +48,8 @@ public class CrearMenu extends AppCompatActivity {
     private String app_key= "70a3be442007e101a7617c95f943525a";
    // private ListaRecipeAdapter listaRecipeAdapter;
 
+    private String health = "";
+    private double kcal_dia = 0;
     private double grasas;
     private double proteinas;
     private double hidratos;
@@ -54,6 +68,12 @@ public class CrearMenu extends AppCompatActivity {
     private String prim_platos[] = new String[7];
     private String seg_platos[] = new String[7];
 
+    private String ingrediente_principal = "palabra";
+
+    private int pescados = 0;
+    private int legumbres = 0;
+    private int carnes = 0;
+    private int huevos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +88,57 @@ public class CrearMenu extends AppCompatActivity {
 
     }
 
-    public void Consultar(View view) {
-        metodoprueba();
+    public void Consultar(View view) throws IOException, JSONException {
+
+
+        String jsonString = IOHelper.stringFromAsset(this, "food.json");
+
+        try {
+            //JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray food = new JSONArray(jsonString);
+
+            String result = "";
+            for (int i = 0; i < 7413; i++) {
+                JSONObject ingredientes = food.getJSONObject(i);
+                //new Gson().fromJson(city.toString(), City.class);
+                Log.i(TAG, ingredientes.getString("Category")+" "+i);
+
+            }
+            //txtJson.setText(result);
+            Log.i(TAG, ""+food.length());
+        } catch (Exception e) {
+            Log.d(TAG, e.getLocalizedMessage());
+        }
+
+
+
+
+        /*Log.i(TAG, food.length());
+        FileInputStream fi = openFileInput("food.json");
+        BufferedInputStream bi = new BufferedInputStream(fi);
+        StringBuffer buffer = new StringBuffer();
+        Log.i(TAG, "hola22");
+
+        while (bi.available() != 0){
+            char c = (char)bi.read();
+            buffer.append(c);
+        }
+
+        Log.i(TAG, "hola333");
+        bi.close();
+        fi.close();
+
+        JSONArray jsonArray = new JSONArray(buffer.toString());
+
+        //StringBuffer foodBuffer = new StringBuffer();
+
+        for( int i=0; i<5; i++){
+            String item = jsonArray.getJSONObject(i).getString("Category");
+            Log.i(TAG, item);
+        }*/
+
+
+        //metodoprueba();
     }
 
     public void Buscar(View view) {
@@ -86,8 +155,7 @@ public class CrearMenu extends AppCompatActivity {
         Cursor fila = BaseDeDatos.rawQuery("select health from datosUsuario where id=01", null);
         Cursor fila2 = BaseDeDatos.rawQuery("select kcal_dia, grasas, proteinas, hidratos from macronutrientes where id=01", null);
 
-        String health = "";
-        double kcal_dia = 0;
+
 
 
         if (fila.moveToFirst()) {
@@ -109,9 +177,9 @@ public class CrearMenu extends AppCompatActivity {
         //Log.i(TAG, health);
         BaseDeDatos.close();
 
-        grasas = grasas * 0.5;
-        proteinas = proteinas * 0.5;
-        hidratos = hidratos * 0.5;
+        //grasas = grasas * 0.7;
+        //proteinas = proteinas * 0.7;
+        //hidratos = hidratos * 0.7;
 
 
 
@@ -127,9 +195,9 @@ public class CrearMenu extends AppCompatActivity {
 
         int randomp = (int) (Math.random() * 5);*/
 
-        String q = "salad";
+        String q = "soup";
         //Log.i(TAG, primeros[randomp]);
-        obtenerPrimeros(q, health, kcal_dia);
+        obtenerPrimeros("salad soup");
 
     }
 
@@ -155,18 +223,15 @@ public class CrearMenu extends AppCompatActivity {
 
     ///////////////////////////////////////////////////////////////////////////////
 
-    private void obtenerPrimeros(String q, String health, double kcal_dia) {
+    private void obtenerPrimeros(String q) {
 
         double kcal_primero = (kcal_dia * 0.3) - 40;
         int intkcal_primero =  (int)kcal_primero;
 
         String calories = "0-"+intkcal_primero;
-
-        Log.i(TAG, calories);
-
         RecipeService service = retrofit.create(RecipeService.class);
         Call<Respuesta> RespuestaCall = service.obtenerDatos(q, app_id, app_key, health, calories);
-
+        Log.i(TAG, q);
         RespuestaCall.enqueue(new Callback<Respuesta>() {
             @Override
             public void onResponse(Call<Respuesta> call, Response<Respuesta> response) {
@@ -175,8 +240,10 @@ public class CrearMenu extends AppCompatActivity {
 
                     Respuesta respuesta = response.body();
                     ArrayList<Recipe> listaRecipe = respuesta.getHits();
-                    COUNT_p = listaRecipe.size();
                     listaPrimeros = listaRecipe;
+                    COUNT_p = listaRecipe.size();
+                    Log.i(TAG, "ENTREE");
+                    obtenerSegundos("main dish");//main dish
 
                 } else {
                     Log.e(TAG, " onResponse: " + response.errorBody());
@@ -190,12 +257,10 @@ public class CrearMenu extends AppCompatActivity {
             }
         });
 
-        q = "main dish";
-        obtenerSegundos(q, health, kcal_dia);
 
     }
 
-    private void obtenerSegundos(String q, String health, double kcal_dia) {
+    private void obtenerSegundos(String q) {
 
         double kcal_segundo = (kcal_dia * 0.7) - 50;
         int intkcal_segundo =  (int)kcal_segundo;
@@ -237,7 +302,7 @@ public class CrearMenu extends AppCompatActivity {
         int random1 = (int) (Math.random() * COUNT_p);
         int random2 = (int) (Math.random() * COUNT_s);
 
-        Log.i(TAG, "Numeros: "+random1+" "+random2);
+        //Log.i(TAG, "Numeros: "+random1+" "+random2);
         Recipe p = listaPrimeros.get(random1);
         Recipe s = listaSegundos.get(random2);
 
@@ -252,9 +317,9 @@ public class CrearMenu extends AppCompatActivity {
         float hidratos_seg = s.getRecipe().getTotalNutrients().getCHOCDF().getQuantity();
 
 
-        Log.i(TAG, "Grasas Tiene: "+ grasas_prim+" + "+grasas_seg + " y son " + grasas);
-        Log.i(TAG, "proteinas Tiene: "+ proteinas_prim+" + "+proteinas_seg + " y son " + proteinas);
-        Log.i(TAG, "hidratos Tiene: "+ hidratos_prim+" + "+hidratos_seg + " y son " + hidratos);
+        //Log.i(TAG, "Grasas Tiene: "+ grasas_prim+" + "+grasas_seg + " y son " + grasas);
+        //Log.i(TAG, "proteinas Tiene: "+ proteinas_prim+" + "+proteinas_seg + " y son " + proteinas);
+       // Log.i(TAG, "hidratos Tiene: "+ hidratos_prim+" + "+hidratos_seg + " y son " + hidratos);
         if (grasas_prim + grasas_seg <= grasas) {
             if (proteinas_prim + proteinas_seg <= proteinas) {
                 if (hidratos_prim + hidratos_seg <= hidratos) {
@@ -271,11 +336,157 @@ public class CrearMenu extends AppCompatActivity {
                             seg_platos[n_semana] = s.getRecipe().getLabel();
                         }else{
                             almacenarmenu();
+                            if(n_semana >= 6) {
+                                metodoprueba();
+                            }
                         }
                     }else{
                         prim_platos[n_semana] = p.getRecipe().getLabel();
                         seg_platos[n_semana] = s.getRecipe().getLabel();
                     }
+
+
+                    for (int i = 0; i< s.getRecipe().getIngredients().size(); i++) {
+
+                        String lineaIngrediente = s.getRecipe().getIngredients().get(i).getText();
+                        String[] parts = lineaIngrediente.split("[ ,\\(\\)\\*\\.-]");
+
+                        for (int j = 0; j < parts.length; j++) {
+                            try {
+                                boolean estaLegumbre = busqueda_legumbres(parts[j].toUpperCase());
+
+                                if (estaLegumbre == true){
+                                    if(legumbres<4){
+                                        legumbres++;
+                                    }else{
+                                        almacenarmenu();
+                                        if(n_semana >= 6) {
+                                            metodoprueba();
+                                        }
+                                    }
+                                }
+
+                                boolean estaPescado = busqueda_pescados(parts[j].toUpperCase());
+                                if (estaPescado == true){
+                                    if(pescados<4){
+                                        pescados++;
+                                    }else{
+                                        almacenarmenu();
+                                        if(n_semana >= 6) {
+                                            metodoprueba();
+                                        }
+                                    }
+                                }
+
+                                boolean estaCarne = busqueda_carnes(parts[j].toUpperCase());
+                                if (estaCarne == true){
+                                    if(carnes<4){
+                                        carnes++;
+                                    }else{
+                                        almacenarmenu();
+                                        if(n_semana >= 6) {
+                                            metodoprueba();
+                                        }
+                                    }
+                                }
+                            } catch (IOException e) {
+                                Log.i(TAG, "ERROR");
+                            }
+                        }
+
+                    }
+
+
+
+                    for (int i = 0; i< p.getRecipe().getIngredients().size(); i++) {
+
+                        String lineaIngrediente = p.getRecipe().getIngredients().get(i).getText();
+                        String[] parts = lineaIngrediente.split("[ ,\\(\\)\\*\\.-]");
+
+                        for (int j = 0; j < parts.length; j++) {
+                            try {
+                                boolean estaLegumbre = busqueda_legumbres(parts[j].toUpperCase());
+
+                                if (estaLegumbre == true){
+                                    if(legumbres<4){
+                                        legumbres++;
+                                    }else{
+                                        almacenarmenu();
+                                        if(n_semana >= 6) {
+                                            metodoprueba();
+                                        }
+                                    }
+                                }
+
+                                boolean estaPescado = busqueda_pescados(parts[j].toUpperCase());
+                                if (estaPescado == true){
+                                    if(pescados<4){
+                                        pescados++;
+                                    }else{
+                                        almacenarmenu();
+                                        if(n_semana >= 6) {
+                                            metodoprueba();
+                                        }
+                                    }
+                                }
+
+                                boolean estaCarne = busqueda_carnes(parts[j].toUpperCase());
+                                if (estaCarne == true){
+                                    if(carnes<4){
+                                        carnes++;
+                                    }else{
+                                        almacenarmenu();
+                                        if(n_semana >= 6) {
+                                            metodoprueba();
+                                        }
+                                    }
+                                }
+                            } catch (IOException e) {
+                                Log.i(TAG, "ERROR");
+                            }
+                        }
+
+                    }
+
+                    double Mgramos = 0;
+                    int n = 0;
+                    for (int i = 0; i< s.getRecipe().getIngredients().size(); i++) {
+                        if (Mgramos < s.getRecipe().getIngredients().get(i).getWeight()){
+                            Mgramos = s.getRecipe().getIngredients().get(i).getWeight();
+                            n = i;
+                        }
+                    }
+                    String ingPrincipal = s.getRecipe().getIngredients().get(n).getText();
+                    String[] parts = ingPrincipal.split("[ ,\\(\\)\\*\\.-]");
+
+
+                    for (int i = 0; i < parts.length; i++) {
+
+                        //Log.i(TAG, parts[i].toUpperCase());
+                        boolean esta = es_igual(parts[i].toUpperCase());
+
+                        /*if (parts[i]=="water"){
+                            almacenarmenu();
+                        }*/
+                        if (esta == true){
+                            //Log.i(TAG, parts[i]);
+                            if(ingrediente_principal!=parts[i]){
+                                ingrediente_principal = parts[i];
+                                //Log.i(TAG, parts[i]);
+                                i=parts.length;
+                            }else{
+                                almacenarmenu();
+                                if(n_semana >= 6) {
+                                    metodoprueba();
+                                }
+                            }
+                        }/*else{
+                            almacenarmenu();
+                        }*/
+
+                        //Log.i(TAG, parts[i]);
+                    }
+
 
                     AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "datos", null, 1);
                     SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
@@ -401,6 +612,108 @@ public class CrearMenu extends AppCompatActivity {
             almacenarmenu();
         }
 
+    }
+
+    private boolean busqueda_legumbres(String palabra) throws IOException {
+
+        //List<String> listado = new ArrayList<String>();
+        String linea;
+        //Log.i(TAG, palabra);
+        InputStream is = this.getResources().openRawResource(R.raw.legumes);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        if(is!=null){
+           // linea=reader.readLine();
+            while ((linea=reader.readLine()) != null){
+                String[] parts = linea.split("[ ,\\(\\)\\*\\.-]");
+                for(int i=0; i< parts.length; i++){
+                    //Log.i(TAG, parts[i]);
+                    if (palabra.equals(parts[i].toUpperCase())){
+                        Log.i(TAG, parts[i]);
+                        linea = null;
+                        return true;
+                    }
+                }
+            }
+
+        }
+        is.close();
+        return false;
+    }
+
+    private boolean busqueda_pescados(String palabra) throws IOException {
+
+        String linea;
+        //Log.i(TAG, palabra);
+        InputStream is = this.getResources().openRawResource(R.raw.pescados);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        if(is!=null){
+            // linea=reader.readLine();
+            while ((linea=reader.readLine()) != null){
+                String[] parts = linea.split("[ ,\\(\\)\\*\\.-]");
+                for(int i=0; i< parts.length; i++){
+                    //Log.i(TAG, parts[i]);
+                    if (palabra.equals(parts[i].toUpperCase())){
+                        Log.i(TAG, parts[i]);
+                        linea = null;
+                        return true;
+                    }
+                }
+            }
+
+        }
+        is.close();
+        return false;
+    }
+
+    private boolean busqueda_carnes(String palabra) throws IOException {
+
+        String linea;
+        //Log.i(TAG, palabra);
+        InputStream is = this.getResources().openRawResource(R.raw.carnes);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        if(is!=null){
+            // linea=reader.readLine();
+            while ((linea=reader.readLine()) != null){
+                String[] parts = linea.split("[ ,\\(\\)\\*\\.-]");
+                for(int i=0; i< parts.length; i++){
+                    //Log.i(TAG, parts[i]);
+                    if (palabra.equals(parts[i].toUpperCase())){
+                        Log.i(TAG, parts[i]);
+                        linea = null;
+                        return true;
+                    }
+                }
+            }
+
+        }
+        is.close();
+        return false;
+    }
+
+    private boolean es_igual(String palabra){
+
+
+        String jsonString = IOHelper.stringFromAsset(this, "food.json");
+
+        try {
+            //JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray food = new JSONArray(jsonString);
+
+            String result = "";
+            for (int i = 0; i < 7413; i++) {
+                JSONObject ingredientes = food.getJSONObject(i);
+                if(palabra.equals(ingredientes.getString("Category"))){
+                    //Log.i(TAG, palabra);
+                    return true;
+                }
+            }
+            return false;
+            //Log.i(TAG, ""+food.length());
+        } catch (Exception e) {
+            Log.d(TAG, e.getLocalizedMessage());
+        }
+        //Log.i(TAG, palabra);
+        return false;
     }
 
     private void metodoprueba() {
